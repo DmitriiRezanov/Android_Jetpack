4c7052ad4599cc152e80368f5ef74ab4813a925c
newdir <dimarezanov1@gmail.com>
2023-01-04 19:18:24 +0300

    Added inflate ViewBinding to layout from fragments.

--- app/src/main/java/com/example/androidjetpack/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/androidjetpack/presentation/WelcomeFragment.kt
@@ -6,16 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.androidjetpack.R
+import com.example.androidjetpack.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+    get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding = null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener{
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
