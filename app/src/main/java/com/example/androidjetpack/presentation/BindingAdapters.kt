071808c877a90c14668ec09f46370e7b2e43e39d
newdir <dimarezanov1@gmail.com>
2023-01-12 13:53:43 +0300

    Added bindingAdapters in GameFragment.kt. Created onClickListener in BindingAdapters. LiveData with bindAdapter.

--- app/src/main/java/com/example/androidjetpack/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/androidjetpack/presentation/BindingAdapters.kt
@@ -1,10 +1,20 @@
 package com.example.androidjetpack.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.view.View.OnClickListener
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.androidjetpack.R
 import com.example.androidjetpack.domain.entity.GameResult
+
+interface OnOptionClickListener {
+
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -59,3 +69,39 @@ private fun getSmileResId(winner: Boolean): Int {
     }
 }
 
+@BindingAdapter("tvSum")
+fun bindTVSum(textView: TextView, sum: Int) {
+    textView.text = sum.toString()
+}
+
+@BindingAdapter("tvLeftNumber")
+fun bindTVLeftNumber(textView: TextView, visibleNumber: Int) {
+    textView.text = visibleNumber.toString()
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context ,enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener){
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
